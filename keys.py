365034d9efb1bece2ade07c2ee856bed2828a49b
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2022-07-06 18:49:20 +0100

    Refactored the code base

---
+++ keys.py
@@ -0,0 +1,12 @@
+access_token = "your access token"
+access_token_secret = "token access token secret"
+consumer_key = "your consumer key"
+consumer_secret = "your consumer secret"
+bearer_token = "your bearer token"
+
+user = 'specify your user'
+password = 'password for the specified user'
+database = 'specify your database'
+charset = 'utf8'
+host = 'specify your host'
+table = 'specify your table'
